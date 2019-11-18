0e326c2859467a98325bb6879b1bbab9cef90cc0
Prathyusha Jaddu <pratz@Prathyushas-MacBook-Air.local>
2019-11-18 17:00:05 -0500

    merge

--- server.js
+++ server.js
@@ -6,15 +6,6 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-
-app.listen(port, () => {
-    console.log("listening", port);
-  });
-  app.get("/", (req, res) => {
-    res.send("its working");
-  });
-//console.log("welcome to react app");
-
 // =======================================
 //              MIDDLEWARE
 // =======================================
