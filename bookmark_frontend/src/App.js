7d03b73f6b2a39ee384a876671f2e196e99062f9
Mausumi Patra <someepatra@gmail.com>
2019-11-19 09:29:35 -0500

    updated branch

--- bookmark_frontend/src/App.js
+++ bookmark_frontend/src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import axios from 'axios';
+import NewBookmarkForm from './components/NewBookmarkForm.js';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let baseURL = process.env.REACT_APP_BASEURL
+
+if (process.env.NODE_ENV === 'development') {
+  baseURL = 'http://localhost:3003'
+} else {
+  baseURL = 'https://fathomless-sierra-68956.herokuapp.com'
 }
+class App extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      bookmarks: []
+    }
+    this.getBookmarks = this.getBookmarks.bind(this);
+  }
+
+  async getBookmarks() {
+    const ownApiData = await axios(`${baseURL}/bookmark`);
+    const data = ownApiData.data;
+    this.setState({
+      bookmarks: data
+    })
+    console.log('bookmark data..'+ this.state.bookmarks)
+  }
+
+  async componentDidMount() {
+    this.getBookmarks();
+  }
+
+  render() {
+    return (
+      <div className="App">
+          <h1> BOOKMARKS APP</h1>
+          <NewBookmarkForm baseURL={baseURL} getBookmarks={this.getBookmarks}/>
+          <div>
+            {this.state.bookmarks.map(bookmark =>  {
+              return  (
+              <p key={bookmark._id}>{bookmark.title}</p>
+                    
+                )
+            })}
+          </div>
+      </div>
+    )
+  }
+
+}
+
 
 export default App;
---
+++ bookmark_frontend/src/components/NewBookmarkForm.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+class NewBookmarkForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "",
+      url: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.currentTarget.name]: event.currentTarget.value
+      //[event.currentTarget.name]: event.currentTarget.value
+    });
+  }
+  async handleSubmit(event) {
+    const baseURL = this.props.baseURL;
+    const response = await axios.post(`${baseURL}/bookmark`, {
+      title: this.state.title
+    });
+    this.setState({
+      title: "",
+      url: ""
+    });
+
+    this.props.getBookmarks();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label htmlFor="title"></label>
+        <input
+          type="text"
+          id="title"
+          name="title"
+          onChange={this.handleChange}
+          value={this.state.title}
+          placeholder="add a bookmark"
+        />
+        {/* <label htmlFor="url"></label>
+            <input type="text" id="url" name="url" onChange={this.handleChange} value={this.state.url} placeholder="add a URL"/> */}
+
+        <input type="submit" value="Add!" />
+      </form>
+    );
+  }
+}
+
+export default NewBookmarkForm;
